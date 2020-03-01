90368bdeb73723d109be1ae4d9f09857929991ea
realvictorprm <mueller.vpr@gmail.com>
2020-03-01 22:41:11 +0100

    Print json formatted string out

--- MyriadTesting/Example/Program.fs
+++ MyriadTesting/Example/Program.fs
@@ -3,6 +3,21 @@
 open System
 open Model
 
+open JsonPlugin.Types
+
+let rec jsonToString json =
+    match json with
+    | JsonValue.Record fields ->
+        seq {
+            for (fieldName, fieldValue) in fields -> sprintf "\"%s\":%s" fieldName (jsonToString fieldValue)
+        } |> String.concat ","
+        |> sprintf "{%s}"
+    | JsonValue.Number number -> sprintf "%f" number 
+    | JsonValue.String str -> sprintf "%A" str
+    | JsonValue.Null -> "null"
+    | JsonValue.Boolean b -> sprintf "%A" b
+    | JsonValue.Float f -> sprintf "%f" f
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
@@ -10,6 +25,11 @@ let main argv =
       bar = ""
       spam = 2. }
     |> Test.Serializer.Serialize
-    |> printfn "%A"
+    |> jsonToString
+    |> printfn "%s"
+    SomeUnion.CaseA 10
+    |> Test.Serializer.Serialize
+    |> jsonToString
+    |> printfn "%s"
     Console.ReadKey() |> ignore
     0 // return an integer exit code
